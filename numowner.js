8a0b660195e75261220b1a1802b25df64f6fbf19
Pickbot <alphalgd@gmail.com>
2021-03-08 16:39:39 -0300

    First commit

---
+++ numowner.js
@@ -0,0 +1,5 @@
+const ownerNumber = ["5519999897566@s.whatsapp.net"]
+const OriginalOwner = '5519999897566'
+
+exports.OriginalOwner = OriginalOwner
+exports.ownerNumber = ownerNumber
---
+++ src/textcomands.js
@@ -0,0 +1,15 @@
+const textcomands = (p) => {
+return `MENU DE TEXTO DO
+💎 ▁▂▃༻ CORNO Bot ༺▃▂▁ 💎
+╔════ ❯ Cmd de texto ❮ ══════════
+║ ❯ *${p}glitch* <text1> | <text2>
+║ ❯ *${p}ravetext* <text1> | <text2>
+║ ❯ *${p}woodtext* <text1> | <text2>
+║ ❯ *${p}neon* <text1> | <text2>
+║ ❯ *${p}sunset* <text1> | <text2>
+║ ❯ *${p}gimage* <text>
+║ ❯ *${p}whatis* <text>
+║ ❯ *${p}text3d* <text>
+╚═══════════════════════════`
+}
+exports.textcomands = textcomands
